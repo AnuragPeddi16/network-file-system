0c0690a3eaecc8d60b2bf7d1610fd71b79b41de8
AnuragPeddi16 <anuragpeddi16@gmail.com>
2024-11-19 21:57:32 +0530

    added ss ack

--- naming_server/naming.c
+++ naming_server/naming.c
@@ -157,6 +157,13 @@ void *accept_ss_connections(void *args) {
         sscanf(buffer+9, "%d:%[^\n]", &client_port, paths);
 
         add_storage_server(ss_fd, inet_ntoa(ss_addr.sin_addr), ntohs(ss_addr.sin_port), client_port, paths);
+
+        int ack = htonl(ACK);
+        if (send(ss_fd, &ack, sizeof(ack), 0) < 0) {
+
+            print_error("sending ack to ss failed");
+
+        }
         
     }
 
